045b95d41575f8b408a75d6d5839fe2258600126
thanhtrung2511 <18520179@gm.uit.edu.vn>
2021-06-06 22:44:22 +0700

    Add messageCard and  update Conversation

--- TickNTalk/screens/ScreenConversations.js
+++ TickNTalk/screens/ScreenConversations.js
@@ -1,13 +1,118 @@
-import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
-import * as styles from '../shared/styles'
+import React from "react";
+import { Layout, Text } from "@ui-kitten/components";
+import * as styles from "../shared/styles";
+import { Image, ScrollView, ImageBackground, SafeAreaView } from "react-native";
+import { Styles, sizeFactor } from "../styles/Styles";
+import { SearchBar } from "react-native-elements";
 
-const ScreenConversations = () => {
+const BasicImage = (props) => {
   return (
-    <Layout style={styles.center}>
-      <Text>conversations Screen!</Text>
+    <Image
+      style={[
+        {
+          width: props.Icon,
+          height: props.Icon,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: props.Round,
+        },
+        props.style,
+      ]}
+      source={props.source}
+    />
+  );
+};
+const MessageCard = (props) => {
+  return (
+    <Layout
+      {...props}
+      style={[Styles.MessageCard, props.containerStyle]}
+      onPress={props.onPress}
+      enabled={props.touchable}
+      onLongPress={props.onLongPress}
+    >
+      <BasicImage
+        Icon={60}
+        Icon={props.ImageSize}
+        source={{ uri: props.ImageSource }}
+        Round={100}
+      ></BasicImage>
+      <Layout
+        style={[
+          {
+            paddingTop: 5,
+            paddingLeft: 5,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          },
+          props.textContainerStyle,
+        ]}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: sizeFactor,
+            color: "black",
+          }}
+        >
+          {props.Name}
+        </Text>
+        <Text
+          style={{
+            width: sizeFactor * 19,
+            fontWeight: props.isRead ? "normal" : "bold",
+          }}
+          numberOfLines={1}
+          ellipsizeMode={"tail"}
+        >
+          {props.LastestChat}
+        </Text>
+      </Layout>
     </Layout>
   );
-}
+};
+const ScreenConversations = () => {
+  return (
+    <Layout style={{ flex: 1 }}>
+      <ImageBackground
+        source={require("../assets/bg.png")}
+        style={{ flex: 1, resizeMode: "cover" }}
+      >
+        <SafeAreaView style={{ flex: 1 }}>
+          <Layout style={{ flex: 1 }}>
+            <ScrollView>
+              <Layout style={[styles.center]}>
+              <SearchBar
+                platform="ios"
+                placeholder="Tìm bạn bè..."
+                lightTheme="true"
+                containerStyle={{
+                  marginHorizontal: 8,
+                  backgroundColor: "transparent",
+                  width: "95%",
+                }}
+                inputContainerStyle={{
+                  backgroundColor: "whitesmoke",
+                  borderRadius: 23,
+                }}
+                leftIconContainerStyle={{ marginLeft: 16 }}
+                inputStyle={{}}
+                placeholder="Tìm kiếm bạn bè.."
+                // onChangeText={(Text) => {
+                //   this.setState({ toSearchText: Text });
+                //   this.onChangeSearchText(Text);
+                // }}
+                // value={this.state.toSearchText}
+              />
+                {/*  Binding message list */}
+                <MessageCard Name="Chó" LastestChat="aaa" ImageSource="../assets/bg.png"/>
+              </Layout>
+            </ScrollView>
+          </Layout>
+        </SafeAreaView>
+      </ImageBackground>
+    </Layout>
+  );
+};
 
 export default ScreenConversations;
--- TickNTalk/styles/Styles.js
+++ TickNTalk/styles/Styles.js
@@ -1,4 +1,10 @@
 import { StyleService } from "@ui-kitten/components";
+import {Dimensions} from 'react-native'
+ const windowWidth = Dimensions.get("window").width;
+ const windowHeight = Dimensions.get("window").height;
+
+//UNIVERSAL SIZE UNIT
+export const sizeFactor = windowWidth / 25.7;
 
 export const Styles = StyleService.create({
     overall: {
@@ -22,5 +28,15 @@ export const Styles = StyleService.create({
         justifyContent: "space-between",
         backgroundColor: "transparent",
         alignSelf: "stretch",
-    }
+    },
+    MessageCard: {
+        paddingTop: sizeFactor * 0.25,
+        paddingBottom: sizeFactor * 0.25,
+        paddingHorizontal: sizeFactor,
+        width: windowWidth,
+        borderRadius: 15,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
 });
